99e2b8a9900f247e93c6dfca931f68c6a03f684d
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-09-05 15:32:04 +0900

    first commit

---
+++ SpeechRecognitionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRecognitionExample
+//
+//  Created by TokyoYoshida on 2020/09/05.
+//  Copyright © 2020 TokyoMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
